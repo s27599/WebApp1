2de55ea04e5c30e4d8d0c165f066d916a2a63240
Wiktor Kaczor <s27599@pjwstk.edu.pl>
2024-04-08 11:44:55 +0200

    1get added

---
+++ WebApplication1/WebApplication1/Controllers/AnimalController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers;
+
+[Route("api/animal")]
+[ApiController]
+public class AnimalController : ControllerBase  
+{
+    [HttpGet]
+    public Animal getAnimals()
+    {
+        return new Animal(1, "jake", Categories.Dog, 123.3, "red");
+    }
+    
+    
+}
---
+++ WebApplication1/WebApplication1/Models/Animal.cs
@@ -0,0 +1,22 @@
+﻿namespace WebApplication1.Models;
+
+public class Animal
+{
+    public int Id { get; set; }
+    public string Imie { get; set; }
+    public Categories Category { get; set; }
+    public double Mass { get; set; }
+    public string Color { get; set; }
+
+    public Animal(int id, string imie, Categories category, double mass, string color)
+    {
+        Id = id;
+        Imie = imie;
+        Category = category;
+        Mass = mass;
+        Color = color;
+    }
+
+
+   
+}
